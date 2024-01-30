1a2164b2185ba86f0bd352a4b2e60b98ee657c1d
HarithaML <59036134+HarithaML@users.noreply.github.com>
2024-01-30 13:13:29 -0800

    Category and Categories

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Categories from './components/Categories';
+
+const App = () => {
+  return (
+    <div>
+      <Categories/>
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.js",
+    "./src/**/*.jsx",
+    "./public/index.html",
+    // Add more file paths if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
